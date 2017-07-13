ba2faf205b06a5aa61dd496aafdbc4f69d2a28b3
suifeng123 <wangshengwen31@gmail.com>
2017-07-13 08:04:24 +0800

    提交Node.js代码

--- cloure.js
+++ cloure.js
@@ -4,3 +4,11 @@
 })()
 console.log(x);
 console.log(y);
+
+var CACHE_TIME = 60*60*24*365;
+if(mimeConf[extname]) {
+   var expires = new Date();
+   expires.setTime(expires.getTime() + CACHE_TIME * 1000);
+   response.setHeader("Expires",expires.toUTCString());
+   response.setHeader('Cache-Control','max-age='+CACHE_TIME);
+}
