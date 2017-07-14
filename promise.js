051602cc853e78cfd6b7df2655da162246596060
suifeng123 <wangshengwen31@gmail.com>
2017-07-14 10:32:31 +0800

    提交部分信息

--- promise.js
+++ promise.js
@@ -13,3 +13,104 @@ const sequenceMiddleware = ({dispatch,getState}) => next => action => {
         });
         },Promise.resolve());
 }
+
+
+/*
+Layouts
+ */
+const layout = ({children} => {
+        <div className='container'>
+           <Header />
+           <div className="content">
+              {children}
+           </div>
+       </div>
+    })
+
+/*
+Views
+ */
+@connect((state) => {
+    //...
+})
+
+class HomeView extends Component {
+    render() {
+        const { sth,changeType } = this.props;
+        const cardProps = { sth,changeType };
+
+        return (
+            <div className="page page-home">
+               <Card {...cardProps} />
+            </div>
+        )
+
+}
+
+class Card extends Components {
+    constructor(props){
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(opts) {
+        const {type} = opts;
+
+        this.opts.changeType(type);
+    }
+
+    render() {
+        const {sth} = this.props;
+
+        return (
+            <div className="mod-card">
+               <Switch onChange={this.handleChange}>
+                //...
+               </Switch>
+            </div>
+        )
+    }
+}
+}
+
+var path = require('path');
+
+module.exports = {
+    entry:'src/app.js',
+    output: {
+        path:  path.join(__dirname,'build'),
+        filename:'app.bundle.js',
+        publicPath: '/build/',
+    },
+    module: {
+        loaders:[{
+            test:/\.js$/,
+            include:path.join(__dirname,'src'),
+            loader:'babel',
+        }]
+    }
+
+}
+
+class Nav extends Component {
+    render() {
+        return (
+            <nav>
+              <Link to="/">Home</Link>
+            </nav>
+        )
+    }
+}
+
+class Frame extends Component {
+    render() {
+        return (
+            <div className="frame">
+               <section className="header">
+                 <Nav />
+               </section>
+            </div>
+        )
+    }
+}
