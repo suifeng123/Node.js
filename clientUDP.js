7a91058323a3eb1fb3e09c32173dade334fbcaec
suifeng123 <wangshengwen31@gmail.com>
2017-07-23 16:17:37 +0800

    提交node模块

--- clientUDP.js
+++ clientUDP.js
@@ -58,3 +58,79 @@ module.exports = function(){
 	}
 
 }
+
+//回调地狱
+A(a,function(b){
+	B(c,function(c){
+		C(d,function(e){
+			//do some thing
+		})
+	})
+})
+
+var fs = require('fs');
+fs.exists('index.txt',function(stats){
+	if(stats){
+		fs.rename('index.txt','test.txt',function(err){
+			fs.unlink('test.txt',function(err){
+				if(err) console.log(err);
+				else console.log('delete success');
+			})
+		})
+	}
+})
+
+mysql = require('mysql');
+/*创建mysql连接对象*/
+var connection = require('mysql');
+//创建mysql连接对象
+var connection = mysql.createConnection({
+	host: 'example.org',
+	user: 'bob',
+	password: 'secret'
+});
+
+connection.connect(function(err){
+	//connected  (unless 'err')
+	
+});
+
+connection.end(function(err){
+  //The connection is terminated now
+});
+
+this.error = function(errNum,constroller,logInfo){
+	var errType = 'error';
+	log(errType,errNum,constroller,logInfo);
+}
+
+//for info log
+this.info = function(errNum,constroller,logInfo){
+	var errType = 'info';
+	log(errType,errNum,constroller,logInfo);
+}
+
+this.warn = function(errNum,controller,logInfo){
+	var errType = 'warn';
+	log(errType,errNum,controller,logInfo);
+}
+
+this.debug = function(errNum,controller,logInfo){
+	var errType = 'debug';
+	log(errType,errNum,controller,logInfo);
+}
+
+//add log in log file
+function log(errType,errorCode,controller,otherInfo){
+	var otherInfo = otherInfo ? otherInfo : {};
+	//错误日志
+	var errorMsg = appLog.getMsg(errorMsg);
+	//记录日志的文件名
+	var  errorLog = getLogFileName(errType,condtroller);
+	log4js.addAppender(lib.log4js.appenders.file(errorLog),loguser);
+	var jsonStr = JSON.stringify(otherInfo);
+	errorMsg = '[code' + errorCode + ']' +'[msg'+errorMsg + ']'+jsonStr;
+	//记录日志
+	addLog(errType,errorMsg);
+	log4js.clearAppenders();
+}
