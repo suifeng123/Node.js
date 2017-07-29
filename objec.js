190fc67a9d01983350860393f9e26b4cbad83a21
suifeng123 <wswen1220@163.comm>
2017-07-29 13:37:17 +0800

    提交正则表达式的一些代码

--- objec.js
+++ objec.js
@@ -2,3 +2,27 @@ function  add([x,y]) {
 	return x + y;
 }
 add([1,2]);
+
+//函数参数的默认值
+jQuery.ajax = function(url,{
+	async = true,
+	beforeSend = function(){},
+	cache = true,
+	complete = function(){},
+	crossDomain = false,
+	global = true,
+	//..more config
+}){
+	//do stuff
+};
+//String.raw
+//String.raw`Hi\u000A`  模板字符串的处理函数，返回一个反斜杠都会转义的字符串，
+//代码实现如下
+String.raw = function(string,...values){
+	var output = "";
+	for(var index=0;index<values.length;index++){
+		output += string.raw[index] + values[index];
+	}
+	output += string.raw[index];
+	return output;
+}
